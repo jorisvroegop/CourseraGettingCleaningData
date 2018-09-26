1447f91109cb4ef5af280e32ec440ca2da8c3d16
Joris Vroegop <jvroegop@deloitte.nl>
2018-09-26 16:50:48 +0200

    Reading data

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,20 @@
 library(xlsx)
 
-path = "C:/Users/jvroegop/Documents/Coursera/Course 3/Week 4" 
-setwd(path)
-datafolder <- "/UCI HAR Dataset"
-testfolder <- "/test"
-trainfolder <- "/train"
+rm(list=ls()) # Empty workspace
 
-current_folder = paste(path, datafolder, testfolder, sep = "")
-available_files = list.files(current_folder)
+data_path = "C:/Users/jvroegop/Documents/Coursera/Course 3/UCI HAR Dataset" 
+setwd(data_path)
+available_files = list.files(data_path)
 
-data <- read.csv(paste(current_folder, available_files[4], sep = "/"), sep = " ")
+features = read.table('./features.txt',header=FALSE)
+activity_type = read.table('./activity_labels.txt',header=FALSE)
 
+# Reading train data
+subject_train = read.table('./train/subject_train.txt',header=FALSE)
+x_train = read.table('./train/x_train.txt',header=FALSE)
+y_train = read.table('./train/y_train.txt',header=FALSE)
+
+# Reading test data
+subject_test = read.table('./test/subject_test.txt',header=FALSE)
+x_test = read.table('./test/x_test.txt',header=FALSE)
+y_test = read.table('./test/y_test.txt',header=FALSE)
